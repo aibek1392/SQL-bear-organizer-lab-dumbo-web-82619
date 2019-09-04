27182535ec3395502749141e2978899fa4ab55f8
Aibek Ozhorov <ozhorov@gmail.com>
2019-09-04 20:48:25 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,6 +1,6 @@
 def selects_all_female_bears_return_name_and_age
   "Write your SQL query here"
-SELECT name ,age, and FROM bears WHERE gender = "F"
+"SELECT name ,age  FROM bears WHERE gender = 'F'"
   
 end
 
