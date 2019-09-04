7a83fa8c3e4e634ace43cec31a3a12ded9da396c
Aibek Ozhorov <ozhorov@gmail.com>
2019-09-04 21:21:08 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -16,7 +16,7 @@ end
 
 def selects_oldest_bear_and_returns_name_and_age
   "Write your SQL query here"
-  "SELECT name, age  FROM  bears WHERE 
+  "SELECT name, age  FROM  bears ORDER BY age DESC; "
 end
 
 def select_youngest_bear_and_returns_name_and_age
