cf02958ac4c7396113bafea45948c2fcf7ee486d
Aibek Ozhorov <ozhorov@gmail.com>
2019-09-04 21:42:50 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -32,7 +32,7 @@ end
 
 def counts_number_of_bears_with_goofy_temperaments
   "Write your SQL query here"
-  "SELECT , COUNT(breed) FROM cats GROUP BY breed"
+  "SELECT breed, COUNT(breed) FROM cats GROUP BY breed"
 end
 
 def selects_bear_that_killed_Tim
